b8477c366d62b3ad084ee4ef929f9164ad6a6623
ivanlie <ivan.lie@meditap.id>
2024-09-11 16:54:57 +0700

    add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from '@/components/ui/button'
+
 const Home = () => {
-  return <div className="text-3xl">Homepage</div>
+  return (
+    <div>
+      <h1 className="text-3xl">Homepage</h1>
+      <Button variant="outline" size="lg">
+        Test
+      </Button>
+    </div>
+  )
 }
 
 export default Home
